3cac4de8283a42224f23321268faaf3ffbb70b96
Yu Zhe <kelvinlim_98@live.com>
2023-06-21 17:56:27 +0800

    Add selected profile state to profile reducer

--- src/features/profile/profileSlice.js
+++ src/features/profile/profileSlice.js
@@ -9,20 +9,29 @@ export const profileSlice = createSlice({
             name: 'Profile 1',
         }
     ],
+    selectedProfileID: 1,
   },
   reducers: {
     add: (state, action) => {
         state.profiles.unshift(action.payload);
+        state.selectedProfileID = action.payload.id;
     },
     update: (state, action) => {
         state.profiles = state.profiles.map(p => p.id === action.payload.id ? action.payload : p);
     },
     remove: (state, action) => {
+        if (state.profiles.length === 1) {
+            return;
+        }
         state.profiles = state.profiles.filter(p => p.id !== action.payload);
+        state.selectedProfileID = state.profiles[0].id;
     },
+    selectProfile: (state, action) => {
+        state.selectedProfileID = action.payload;
+    }
   },
 })
 
-export const { add, update, remove } = profileSlice.actions
+export const { add, update, remove, selectProfile } = profileSlice.actions
 
 export default profileSlice.reducer
--- src/features/profile/profileSlice.test.js
+++ src/features/profile/profileSlice.test.js
@@ -1,28 +1,40 @@
-import reducer, { add, update, remove } from './profileSlice'
+import reducer, { add, update, remove, selectProfile } from './profileSlice'
 
 test('should return the initial state', () => {
-    expect(reducer(undefined, { type: undefined })).toEqual({ profiles: [{ id: 1, name: 'Profile 1' }] })
+    expect(reducer(undefined, { type: undefined })).toEqual({ profiles: [{ id: 1, name: 'Profile 1' }], selectedProfileID: 1 })
 })
 
 test('should handle add', () => {
-    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }] }, add({ id: 2, name: 'Profile 2' }))).toEqual({
+    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }], selectedProfileID: 1 }, add({ id: 2, name: 'Profile 2' }))).toEqual({
         profiles: [{ id: 2, name: 'Profile 2' }, { id: 1, name: 'Profile 1' }],
+        selectedProfileID: 2,
     })
 })
 
 test('should handle update', () => {
-    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }] }, update({ id: 1, name: 'Profile 2' }))).toEqual({
-        profiles: [
-            {
-                id: 1,
-                name: 'Profile 2',
-            }
-        ],
+    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }], selectedProfileID: 1 }, update({ id: 1, name: 'Profile 2' }))).toEqual({
+        profiles: [{ id: 1, name: 'Profile 2' }],
+        selectedProfileID: 1,
     })
 })
 
 test('should handle remove', () => {
-    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }, { id: 2, name: 'Profile 2' }] }, remove(2))).toEqual({
+    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }, { id: 2, name: 'Profile 2' }], selectedProfileID: 2 }, remove(2))).toEqual({
         profiles: [{ id: 1, name: 'Profile 1' }],
+        selectedProfileID: 1,
+    })
+})
+
+test('should handle remove with only one profile', () => {
+    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }], selectedProfileID: 1 }, remove(1))).toEqual({
+        profiles: [{ id: 1, name: 'Profile 1' }],
+        selectedProfileID: 1,
+    })
+})
+
+test('should handle select profile', () => {
+    expect(reducer({ profiles: [{ id: 1, name: 'Profile 1' }, { id: 2, name: 'Profile 2' }], selectedProfileID: 2 }, selectProfile(1))).toEqual({
+        profiles: [{ id: 1, name: 'Profile 1' }, { id: 2, name: 'Profile 2' }],
+        selectedProfileID: 1,
     })
 })
